b235b7554f175a6519240aaf3415d9b837ac2283
Angus Hollands <goosey15@gmail.com>
2021-05-10 10:15:39 +0100

    Refactor: don't test for .pyenv
    Rather than testing for software, just don't stow the .pyenv configuration if it isn't required.

--- pyenv/.zshrc.d/0100-pyenv.zsh
+++ pyenv/.zshrc.d/0100-pyenv.zsh
@@ -1,4 +1,2 @@
-if [[ -d "$HOME/.pyenv" ]]; then
-	zinit ${ZINIT_WAIT-wait} lucid for \
-	    atload'eval "$(pyenv virtualenv-init - zsh)"' OMZP::pyenv/pyenv.plugin.zsh
-fi
+zinit ${ZINIT_WAIT-wait} lucid for \
+ OMZP::pyenv/pyenv.plugin.zsh
