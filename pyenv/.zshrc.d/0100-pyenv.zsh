a7195cb60409060bda7ff50b7f1488fdad018ab8
Angus Hollands <goosey15@gmail.com>
2021-05-17 07:05:17 +0100

    Refactor: change plugin priorities
    Speed up pyenv loading (remove rehash)

--- pyenv/.zshrc.d/0100-pyenv.zsh
+++ pyenv/.zshrc.d/0100-pyenv.zsh
@@ -1,6 +1,6 @@
 # Load pyenv hook by compiling a different file to the no-op URL
 zinit ${ZINIT_WAIT-wait} lucid light-mode for \
-    atclone'cat <(pyenv init -) <(pyenv virtualenv-init -) > zhook.zsh' pick="zhook.zsh" nocompile'!' \
+    atclone'cat <(pyenv init - --no-rehash) <(pyenv virtualenv-init -) > zhook.zsh' pick="zhook.zsh" nocompile'!' \
     atpull'%atclone' \
     id-as"pyenv" \
     https://gist.github.com/agoose77/0a3f6c4527272ad06afd1a0788104280/raw
