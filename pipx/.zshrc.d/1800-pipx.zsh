baf2a38f07fb2b2e74c6d24440f0021b317431fd
Angus Hollands <goosey15@gmail.com>
2021-05-24 10:32:05 +0100

    Refactor: use zdharma/null repo

--- pipx/.zshrc.d/1800-pipx.zsh
+++ pipx/.zshrc.d/1800-pipx.zsh
@@ -3,4 +3,4 @@ zinit ${ZINIT_WAIT-wait} lucid light-mode for \
     atclone'register-python-argcomplete pipx > zhook.zsh' pick="zhook.zsh" nocompile'!' \
     atpull'%atclone' \
     id-as"pipx" \
-    https://gist.github.com/agoose77/0a3f6c4527272ad06afd1a0788104280/raw
+    zdharma/null
--- podman/.zshrc.d/1800-podman.zsh
+++ podman/.zshrc.d/1800-podman.zsh
@@ -3,4 +3,4 @@ zinit ${ZINIT_WAIT-wait} lucid light-mode for \
     atclone'podman completion zsh > _podman' pick="_podman" nocompile'!' \
     atpull'%atclone' \
     id-as"podman" \
-    https://gist.github.com/agoose77/0a3f6c4527272ad06afd1a0788104280/raw
+    zdharma/null
