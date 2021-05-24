8d618bc8156b47bc071d4f36e40bcb278809d784
Angus Hollands <goosey15@gmail.com>
2021-05-24 13:30:37 +0100

    Feature: add support for conda

--- conda/.zshrc.d/0100-mambaforge.zsh
+++ conda/.zshrc.d/0100-mambaforge.zsh
@@ -1,7 +1,9 @@
 # Load pyenv hook by compiling a different file to the no-op URL
 zinit ${ZINIT_WAIT-wait} lucid light-mode for \
-    atclone'conda shell.zsh hook conda.zsh' \
+    atclone'conda shell.zsh hook > conda.zsh' \
     nocompile'!' \
     pick"conda.zsh" \
     atpull'%atclone' \
+    id-as"conda" \
+    zdharma/null \
     esc/conda-zsh-completion
