ab1bb8b078aed1d6d801d0d07f5cc70628a8b141
Angus Hollands <goosey15@gmail.com>
2021-11-04 11:16:52 +0000

    Feat: use standard pipx config (so can use to launch default shell)

--- pipx/.profile.d/050-pipx.sh
+++
@@ -1,7 +0,0 @@
-export PIPX_HOME="$HOME/.pipx"
-export PIPX_BIN_DIR="$PIPX_HOME/bin"
-export PATH="$PIPX_BIN_DIR:$PATH"
-
-if [[ ! -d "$PIPX_HOME" ]]; then
-    mkdir --parents "$PIPX_HOME" "$PIPX_BIN_DIR"
-fi
